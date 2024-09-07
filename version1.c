f94daba95eef4389c03172b7e59d16ec554069df
Beniamin-Emanuel Imbrescu <160716189+Stikeez14@users.noreply.github.com>
2024-09-07 17:12:22 +0300

    v.0.2: create, init, print and free functions added for for the first game map / arena

--- version1.c
+++ version1.c
@@ -1,6 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+char** create_arena(int rows, int cols) { // allocates memory for the arena
+    char **arena = (char **)malloc(rows * sizeof(char *));
+    for (int i = 0; i < rows; i++){
+        arena[i] = (char *)malloc(cols * sizeof(char));
+    }
+    return arena;
+}
+
+void free_arena(char **arena, int rows) { // frees the allocated memory
+    for (int i = 0; i < rows; i++) {
+        free(arena[i]);
+    }
+    free(arena);
+}
+
+void initialize_arena(char **arena, int rows, int cols) { // initializes the arena by adding elements in the matrix
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            arena[i][j] = '0'; 
+        }
+    }
+}
+
+void print_arena(char **arena, int rows, int cols) { // prints all the matrix elements
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%c ", arena[i][j]);
+        }
+        printf("\n");
+    }
+}
 
 int main(){
-    printf("test\n");
+
+    int rows = 10, cols = 10;
+    char **arena;  // dynamically allocated 2D array 
+
+    arena = create_arena(rows, cols); 
+    initialize_arena(arena, rows, cols);
+    print_arena(arena, rows, cols);
+    free_arena(arena, rows);
+
     return 0;
 }
